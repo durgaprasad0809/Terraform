be64abf2795bf0f6c8a4a800f5bf955512ad9536
durgaprasad0809 <durgaprasad.a0809@gmail.com>
2025-06-09 22:57:49 +0530

    Terraform changes

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,24 @@
 # Create EC2 Instance using Terraform
+resource "aws_instance" "this" {  
+  ami = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids =  [aws_security_group.allow_tls.id]
+  instance_type = "t3.micro"
+  tags = {
+    Name = "terraform-demo" # name of the instances
+  }
+  
+}
 # Create an Security Resource using Terraform
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = "${aws_vpc.main.id}"
-
+  
   ingress {
-    description = "TLS from VPC"
+    description = "Allowing all"
     from_port   = 22 # SSH
     to_port     = 22 # SSH
     protocol    = "tcp" # TCP Protocal
-    cidr_blocks = aws_vpc.main.cidr_block
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
